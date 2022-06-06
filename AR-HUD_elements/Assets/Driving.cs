e8bc98e65b6c11e492185609f816907f1ad1f3c3
4nn1k4 <annika.koehler21@gmail.com>
2022-06-06 19:57:55 +0200

    added_street_signs

--- AR-HUD_elements/Assets/Driving.cs
+++ AR-HUD_elements/Assets/Driving.cs
@@ -5,9 +5,12 @@ using PathCreation;
 
 public class Driving : MonoBehaviour
 {
+    public GameObject[] speedPoints;
     public PathCreator pathCreator;
     public float speed = 7;
     float distanceTravelled;
+    int speedPointCounter = 0;
+    private bool speedChanger = false;
 
 
     // Start is called before the first frame update
@@ -19,6 +22,25 @@ public class Driving : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(speedPointCounter < speedPoints.Length){
+            var distance = Vector3.Distance(transform.position, speedPoints[speedPointCounter].transform.position);
+            if(distance < 2f && (speedPointCounter % 2 == 0))
+            {
+                speed = speed - 0.04f;
+                speedChanger = true;
+            }
+            else if(distance < 2f && (speedPointCounter % 2 == 1))
+            {
+                speed = speed + 0.04f;
+                speedChanger = true;
+            }
+            if(speedChanger && (distance >= 2f))
+            {
+                speedPointCounter++;
+                speedChanger = false;
+            }
+        }
+
         distanceTravelled += speed * Time.deltaTime;
         transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled);
         transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled);
