0e99b7f2ed980b2598b0dcf1beceacb65d7d6570
4nn1k4 <annika.koehler21@gmail.com>
2022-06-11 21:06:32 +0200

    added_2_screen_fix

--- AR-HUD_elements/Assets/Driving.cs
+++ AR-HUD_elements/Assets/Driving.cs
@@ -22,23 +22,26 @@ public class Driving : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(speedPointCounter < speedPoints.Length){
-            var distance = Vector3.Distance(transform.position, speedPoints[speedPointCounter].transform.position);
-            if(distance < 2f && (speedPointCounter % 2 == 0))
-            {
-                speed = speed - 0.04f;
-                speedChanger = true;
-            }
-            else if(distance < 2f && (speedPointCounter % 2 == 1))
-            {
-                speed = speed + 0.04f;
-                speedChanger = true;
-            }
-            if(speedChanger && (distance >= 2f))
-            {
-                speedPointCounter++;
-                speedChanger = false;
-            }
+        
+        var distance = Vector3.Distance(transform.position, speedPoints[speedPointCounter].transform.position);
+        if(distance < 2f && (speedPointCounter % 2 == 0))
+        {
+            speed = speed - 0.04f;
+            speedChanger = true;
+        }
+        else if(distance < 2f && (speedPointCounter % 2 == 1))
+        {
+            speed = speed + 0.04f;
+            speedChanger = true;
+        }
+        if(speedChanger && (distance >= 2f))
+        {
+            speedPointCounter++;
+            speedChanger = false;
+        }
+        if(speedPointCounter == speedPoints.Length)
+        {
+            speedPointCounter = 0;
         }
 
         distanceTravelled += speed * Time.deltaTime;
